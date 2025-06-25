d46d3ba9f794acbdeb289dc3b29ae102c39599b3
james-fleming394 <james.fleming394@gmail.com>
2025-06-25 12:40:27 -0400

    Update: Fix positions of hotspots to overlay on players

--- src/components/stadium.jsx
+++ src/components/stadium.jsx
@@ -9,31 +9,31 @@ export default function Stadium() {
 
         <div className="hotspot-overlay">
             {/* Pitcher’s Mound */}
-            <PlayerHotspot top="42%" left="48%" label="Name & Title" onClick={() => alert('Intro')} />
+            <PlayerHotspot top="62%" left="47%" label="Name & Title" onClick={() => alert('Intro')} />
 
-            {/* Home Plate */}
-            <PlayerHotspot top="68%" left="46%" label="Contact" onClick={() => alert('Contact')} />
+            {/* Catcher */}
+            <PlayerHotspot top="74%" left="3%" label="Contact" onClick={() => alert('Contact')} />
+
+            {/* Hitter */}
+            <PlayerHotspot top="69%" left="9.5%" label="About Me" onClick={() => alert('About Me')} />
 
             {/* First Base */}
-            <PlayerHotspot top="55%" left="70%" label="About Me" onClick={() => alert('About Me')} />
-
-            {/* Second Base */}
-            <PlayerHotspot top="50%" left="58%" label="Project 1" onClick={() => alert('Project 1')} />
+            <PlayerHotspot top="74%" left="71%" label="Project 1" onClick={() => alert('Project 1')} />
 
             {/* Shortstop */}
-            <PlayerHotspot top="50%" left="38%" label="Project 3" onClick={() => alert('Project 3')} />
+            <PlayerHotspot top="56%" left="73%" label="Project 2" onClick={() => alert('Project 2')} />
 
             {/* Third Base */}
-            <PlayerHotspot top="55%" left="28%" label="Project 2" onClick={() => alert('Project 2')} />
+            <PlayerHotspot top="53%" left="54%" label="Project 3" onClick={() => alert('Project 3')} />
 
             {/* Left Field */}
-            <PlayerHotspot top="35%" left="18%" label="GitHub" onClick={() => alert('GitHub')} />
+            <PlayerHotspot top="44%" left="68%" label="GitHub" onClick={() => alert('GitHub')} />
 
             {/* 3rd Base Coach */}
-            <PlayerHotspot top="60%" left="10%" label="LinkedIn" onClick={() => alert('LinkedIn')} />
+            <PlayerHotspot top="54%" left="31.5%" label="Resume" onClick={() => alert('Resume')} />
 
             {/* 1st Base Coach */}
-            <PlayerHotspot top="60%" left="85%" label="Resume" onClick={() => alert('Resume')} />
+            <PlayerHotspot top="82%" left="41.5%" label="LinkedIn" onClick={() => alert('LinkedIn')} />
         </div>
     </div>
     );
